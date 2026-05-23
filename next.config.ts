f7577f551d144abb9527b0a5d7dc6b2bfc174a86
unknown <xxmarvinxx2017@gmail.com>
2026-05-23 08:26:38 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['@prisma/client', 'prisma'],
 };
 
 export default nextConfig;
